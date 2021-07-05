4438d8ace93e6cec9ab4af88473bbdd22ef01dde
Atell <delta_atell@protonmail.com>
2021-07-05 14:57:16 +0300

    Uncomment error printing

--- InterpretationDimensions.jl
+++ InterpretationDimensions.jl
@@ -10,12 +10,12 @@ end
 Dimension(cl::I where I<:Integer, el::I where I<:Integer, ns::Namespace, name::String) = Dimension(cl, cl, el, ns, name)
 
 function dimension_forward!(dim::Dimension)::Nothing
-    #try
+    try
         refline::Ref{UInt} = Ref{UInt}(dim.current_line)
         aetherlang_eval!(lines[dim.current_line], dim.namespace, refline)
         dim.current_line = refline[]+1
-    #catch e
-    #   aetherlang_print_exception(e, lines[dim.current_line], dim.name) # also exits
-    #end
+    catch e
+       aetherlang_print_exception(e, lines[dim.current_line], dim.name) # also exits
+    end
     print() # I don't why but it just doesn't work without this print and exists with "ERROR: LoadError: MethodError: convert(::Type{Union{}}, ::UInt64) is ambiguous."
 end

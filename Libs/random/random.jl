62a24f4cfb2d2f0f5bdb0cae606a8b70f4bdc986
Atell <delta_atell@protonmail.com>
2021-07-16 22:54:57 +0300

    Better Random

--- Libs/random/random.jl
+++ Libs/random/random.jl
@@ -1,9 +1,24 @@
-function random_srand(line::Ref{UInt}, ns::Namespace, seed::AetherlangObject)::AetherlangObject{AetherlangVoid}
+function random_сsrand(line::Ref{UInt}, ns::Namespace, seed::AetherlangObject)::AetherlangObject{AetherlangVoid}
     ccall(:srand, Cvoid, (Int32,), seed.dataref)
     AetherlangObject()
 end
 
+function AE_choose(line::Ref{UInt}, ns::Namespace, arr::AetherlangObject)::AetherlangObject
+    AetherlangObject(rand(arr.dataref))
+end
+
+function AE_shuffled(line::Ref{UInt}, ns::Namespace, arr::AetherlangObject)::AetherlangObject
+    a = copy(arr.dataref)
+    for i in 1:length(a)
+        q::Int = rand(1:length(a))
+        a[i], a[q] = a[q], a[i]
+    end
+    AetherlangObject(a)
+end
+
 random_namespace_modify = Namespace(
-    "c.srand" => AetherlangObject(random_srand),
-    "c.rand" => AetherlangObject((line::Ref{UInt}, ns::Namespace, dummy::AetherlangObject)->AetherlangObject(ccall(:rand, Int32, ())))
+    "c.srand" => AetherlangObject(random_сsrand),
+    "c.rand" => AetherlangObject((line::Ref{UInt}, ns::Namespace, dummy::AetherlangObject)->AetherlangObject(ccall(:rand, Int32, ()))),
+    "choose" => AetherlangObject(AE_choose),
+    "shuffled" => AetherlangObject(AE_shuffled)
 )
